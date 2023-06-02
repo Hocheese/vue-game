fecc82184206fcf8d21ed4d6401f81aa93287713
wangheqing <2653591247@qq.com>
2023-06-03 00:52:37 +0800

    初始化

---
+++ src/components/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>{{ data }}</div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const data = ref('当前时间')
+
+onMounted(() => {
+    setInterval(() => {
+        data.value = '当前时间:'+(new Date())
+    }, 1000)
+})
+</script>
