26d96b173c817f6063120f441c4c71801b69e471
wangheqing <2653591247@qq.com>
2023-06-22 22:51:56 +0800

    草地

---
+++ src/components/playground/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="playground">
+        <div class="top">
+            <div class="cardArea">1111</div>
+        </div>
+        <div class="glass">
+            <div class="row" v-for="row in runtime.lines">
+                <div class="col" v-for=" col in row.blocks">233</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import { reactive } from 'vue';
+const runtime = reactive({
+    lines: []
+});
+
+const line = new Array(5).fill(5)
+runtime.lines= line.map(() => {
+    const blocks = new Array(5).fill(5);
+    return {
+        students: [],
+        blocks: blocks.map(() => {
+            return {
+                type: 1,
+                teachers: []
+            }
+        })
+    }
+})
+
+</script>
+<style lang="scss" scoped>
+.playground{
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+}
+.glass{
+    background-color: greenyellow;
+    .row {
+        display: flex;
+        width: 100vw;
+        height: 10vh;
+        .col {
+            width: 20vw;
+            border: 1px solid green;
+        }
+    }
+}
+</style>
